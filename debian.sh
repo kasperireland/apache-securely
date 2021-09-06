d0a709f66696147ea4a26ff058c7c75315410abc
Kasper Ireland <54741819+kasperireland@users.noreply.github.com>
2021-09-05 22:15:23 -0400

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -4,3 +4,8 @@ apt install apache2
 echo Apache2 Package Installed.
 wget https://raw.githubusercontent.com/Swivro/ddos-protection-script/main/antiddos-debian.sh && chmod +x antiddos-debian.sh && ./antiddos-debian.sh
 echo Anti DDoS Configuration via IpTables Saved.
+echo ~~~~~~~~
+echo You will now configure a TLS/SSL Certificate for your website domain. This will start in 7 seconds.
+sleep 7
+certbot --apache
+echo Congratulations, you are finished with this script.
