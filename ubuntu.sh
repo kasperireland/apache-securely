b23a32e91df317b587f90d542a30c98b5b3dd16c
Kasper Ireland <54741819+kasperireland@users.noreply.github.com>
2021-09-05 22:05:15 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,6 @@
 apt update
+echo Packages updated.
 apt install apache2
+echo Apache2 Package Installed.
 wget https://raw.githubusercontent.com/Swivro/ddos-protection-script/main/antiddos-debian.sh && chmod +x antiddos-debian.sh && ./antiddos-debian.sh
+echo Anti DDoS Configuration via IpTables Saved.
